c078064838710bb425b9110a7339c1b31a369ba1
tauesp127-alexis <gestionti@taurus.com.mx>
2025-01-15 12:25:55 -0600

    Cambio a lenguaje español y vista de auth

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,24 +1,36 @@
 <x-guest-layout>
     <div class="mb-4 text-sm text-gray-600">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+        {{ __('¿Olvidaste tu contraseña? Escribe el correo con el que te registraste y enviaremos un enlace para crear una nueva contraseña') }}
     </div>
 
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <form method="POST" action="{{ route('password.email') }}">
+    <form method="POST" action="{{ route('password.email') }}" novalidate>
         @csrf
 
         <!-- Email Address -->
         <div>
-            <x-input-label for="email" :value="__('Email')" />
+            <x-input-label for="email" :value="__('Correo')" />
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
-        <div class="flex items-center justify-end mt-4">
+        <div class="flex items-center justify-between my-4">
+            <x-link :href="route('login')">
+                ¿Ya tienes cuenta?
+            </x-link>
+
+            <x-link :href="route('register')">
+                Crear cuenta
+            </x-link>
+
+
+        </div>
+
+        <div class="flex items-end justify-end mt-4">
             <x-primary-button>
-                {{ __('Email Password Reset Link') }}
+                {{ __('Enviar instrucciones') }}
             </x-primary-button>
         </div>
     </form>
--- resources/views/components/link.blade.php
+++ resources/views/components/link.blade.php
@@ -1,3 +1,8 @@
-<div>
-    <!-- Simplicity is the ultimate sophistication. - Leonardo da Vinci -->
-</div>
+
+@php
+    $classes = "underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+@endphp
+
+<a {{ $attributes->merge(['class' => $classes]) }}>
+    {{ $slot }}
+</a>
