e07584585def375ee1b56557b257c79fc5db2a11
tauesp127-alexis <gestionti@taurus.com.mx>
2025-01-16 10:38:49 -0600

    Editar elemento

--- app/Livewire/EditarUpload.php
+++ app/Livewire/EditarUpload.php
@@ -8,20 +8,49 @@ use Livewire\WithFileUploads;
 
 class EditarUpload extends Component
 {
+    public $upload_id;
     public $titulo;
     public $imagen;
+    public $imagen_nueva;
 
     use WithFileUploads;
 
 
     protected $rules = [
         'titulo' => 'required|string',
-        'imagen' => 'required|image|max:5120'
+        'imagen_nueva' => 'nullable|image|max:5120'
     ];
 
     public function mount(Upload $upload){
+        $this->upload_id = $upload->id;
         $this->titulo = $upload->titulo;
         $this->imagen = $upload->imagen;
+    }
+
+    public function editarVacante() {
+        $datos = $this->validate();
+
+        // Si hay una nueva imagen
+        if($this->imagen_nueva) {
+            $imagen = $this->imagen_nueva->store('public/uploads');
+            $datos['imagen'] = str_replace('public/uploads/', '', $imagen);
+
+        }
+
+        // Encontrar el Upload a editar
+        $upload = Upload::find($this->upload_id);
+
+        // Asignar los valores
+        $upload->titulo = $datos['titulo'];
+        $upload->imagen = $datos['imagen'] ?? $upload->imagen;
+
+        // Guardar upload
+        $upload->save();
+
+        // Redireccionar
+        session()->flash('mensaje', 'El elemento se actualizó correctamente!');
+
+        return redirect()->route('uploads.index');
     }
 
     public function render()
--- resources/views/livewire/editar-upload.blade.php
+++ resources/views/livewire/editar-upload.blade.php
@@ -1,6 +1,6 @@
 <form 
     class="md:w-1/2"
-    wire:submit.prevent="subirArchivo"
+    wire:submit.prevent="editarVacante"
 >
     <div class="mt-4">
         <x-input-label for="titulo" :value="__('Titulo')" />
@@ -21,16 +21,23 @@
             id="imagen" 
             class="block mt-1 w-full" 
             type="file" 
-            wire:model="imagen" 
+            wire:model="imagen_nueva" 
             accept="image/*"
             />
         <x-input-error :messages="$errors->get('imagen')" class="mt-2" />
 
         <div class="my-5 w-80">
-            <x-input-label :value="__('Imagen actual')" />
-            <div class="border-4 border-gray-700 rounded-lg p-2">
-                <img src="{{ asset('storage/uploads/' . $imagen ) }}" alt="{{ 'Archivo: ' . $titulo }}">
-            </div>
+            @if ($imagen_nueva)
+                <x-input-label :value="__('Nueva Imagen:')" />
+               <div class="border-4 border-gray-700 rounded-lg p-2">
+                   <img src="{{ $imagen_nueva->temporaryUrl() }}" alt=""> 
+               </div>
+            @else
+                <x-input-label :value="__('Imagen actual')" />
+                <div class="border-4 border-gray-700 rounded-lg p-2">
+                    <img src="{{ asset('storage/uploads/' . $imagen ) }}" alt="{{ 'Archivo: ' . $titulo }}">
+                </div>
+            @endif
         </div>
 
     </div>
