5abad399099280c6de00c813e0084f9065341ab0
tauesp127-alexis <gestionti@taurus.com.mx>
2025-01-16 14:13:17 -0600

    Cambios esteticos para mostrar listado e imagenes

--- resources/views/livewire/mostrar-uploads.blade.php
+++ resources/views/livewire/mostrar-uploads.blade.php
@@ -5,14 +5,16 @@
         @forelse ($uploads as $upload)
             <div class="p-6 bg-white border-b border-gray-200 md:flex md:justify-between md:items-center">
                 <div class="leading-10">
-                    <a href="" class="text-xl font-bold">
+                    <a href="{{ route('uploads.edit', $upload->id ) }}" class="text-xl font-bold">
                         {{ $upload->titulo }}
                     </a>
                     <p class="text-sm text-gray-500">Ultima actualización: {{ $upload->updated_at->format('d/m/Y') }}</p>
                 </div>
 
                 <div class="w-60 border-4 border-gray-700 rounded-lg p-2 mx-auto mt-2 md:mt-0">
-                    <img src="{{ asset('storage/uploads/' . $upload->imagen ) }}" alt="{{ 'Archivo: ' . $upload->titulo }}">
+                    <button wire:click="$dispatch('mostrarImg', {{ $upload }})">
+                        <img src="{{ asset('storage/uploads/' . $upload->imagen ) }}" alt="{{ 'Archivo: ' . $upload->titulo }}">
+                    </button>
                 </div>
     
                 <div class="flex flex-col md:flex-row items-stretch gap-3 mt-5 md:mt-0">
@@ -46,28 +48,41 @@
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script>
         document.addEventListener('livewire:initialized', function () {
+            const path = `{{ asset('storage/uploads') }}`
+
             @this.on('eliminar', uploadId => {
                 Swal.fire({
-                title: "¿Eliminar elemento?",
-                text: "Esta acción es irreversible!",
-                icon: "danger",
-                showCancelButton: true,
-                confirmButtonColor: "#3085d6",
-                cancelButtonColor: "#d33",
-                cancelButtonText: "Cancelar",
-                confirmButtonText: "Si, eliminar!"
+                    title: "¿Eliminar elemento?",
+                    text: "Esta acción es irreversible!",
+                    icon: "danger",
+                    showCancelButton: true,
+                    confirmButtonColor: "#3085d6",
+                    cancelButtonColor: "#d33",
+                    cancelButtonText: "Cancelar",
+                    confirmButtonText: "Si, eliminar!"
                 }).then((result) => {
-                if (result.isConfirmed) {
-                    // Eliminar upload
-                    @this.call('eliminarUpload', uploadId)
-                    Swal.fire({
-                        title: "Eliminado!",
-                        text: "El archivo ha sido eliminado.",
-                        icon: "success"
-                    });
-                }
+                    if (result.isConfirmed) {
+                        // Eliminar upload
+                        @this.call('eliminarUpload', uploadId)
+                        Swal.fire({
+                            title: "Eliminado!",
+                            text: "El archivo ha sido eliminado.",
+                            icon: "success"
+                        });
+                    }
+                });
             });
-            })
+
+            @this.on('mostrarImg', ( upload ) => {
+                Swal.fire({
+                    title: upload.titulo,
+                    width: 780,
+                    height: 480,
+                    imageUrl: `${path}/${upload.imagen}`,
+                    imageWidth: 780,
+                    imageHeight: 480
+                });
+            });
         })
     </script>
 
