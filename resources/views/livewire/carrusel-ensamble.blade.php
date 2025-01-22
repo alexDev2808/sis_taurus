04f82d181830418f32a56bf5c0a3effe841e8f67
tauesp127-alexis <gestionti@taurus.com.mx>
2025-01-22 15:22:18 -0600

    Boton para regresar en carrusel

--- resources/views/livewire/carrusel-ensamble.blade.php
+++ resources/views/livewire/carrusel-ensamble.blade.php
@@ -1,11 +1,17 @@
 
 <div>
 
-    <div class="w-8 h-8 absolute top-0 left-0 z-40 flex justify-center items-center">
+    <div class="w-14 h-8 absolute top-0 left-0 z-40 flex justify-center items-center">
+        <a href="{{ route('ensamble.index') }}">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3" />
+            </svg> 
+        </a>   
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
             <path stroke-linecap="round" stroke-linejoin="round" d="M15.91 11.672a.375.375 0 0 1 0 .656l-5.603 3.113a.375.375 0 0 1-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112Z" />
-        </svg>         
+        </svg> 
+        
     </div>
 
     <div class="swiper mySwiper w-full h-screen">
